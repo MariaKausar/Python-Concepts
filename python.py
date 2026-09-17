ba753542deb247ae978749595fad3585c565bc76
MariaKausar <maria553kasuar@gmail.com>
2026-09-18 00:01:31 +0100

    first 3 commands of git initialization

--- python.py
+++ python.py
@@ -1,3 +1,7 @@
 print ("Hi, Maria!")
 print("First I create a repo and then a foulder in my system")
 print("open new terminal, then I will create a new file and write some code in it")
+print("git init ->  initialize a new git repository in my project foulder " \
+"git add . -> add all the files to the staging area | all the changes are ready to commit | prepare for commit" \
+"git commit -m "first commit" -> commit the changes to the local repository | save the changes to the local repository or history | save/history" \
+"All the 3 above commands for commit")
